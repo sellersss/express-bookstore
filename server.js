00edfe48bc4f61cf2f48284e91a6bd57f017aad9
Sellers Crisp <sellershcrisp@gmail.com>
2021-03-25 13:13:29 -0400

    updated setup files

--- server.js
+++ server.js
@@ -4,5 +4,5 @@
 const app = require("./app");
 
 app.listen(3000, () => {
-  console.log(`Server starting on port 3000`);
+  console.log(`Server starting on port 3000 -> http://localhost:3000`);
 });
