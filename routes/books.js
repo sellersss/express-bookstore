f5659982436e902357804a980980593b73578422
Sellers Crisp <sellershcrisp@gmail.com>
2021-03-25 13:12:42 -0400

    Updated books routes with authentication from jsonschema

--- routes/books.js
+++ routes/books.js
@@ -1,12 +1,15 @@
 const express = require("express");
 const Book = require("../models/book");
+const jsonschema = require("jsonschema");
+const bookSchema = require("../schema/bookSchema.json");
+const ExpressError = require("../expressError");
 
 const router = new express.Router();
 
 
 /** GET / => {books: [book, ...]}  */
 
-router.get("/", async function (req, res, next) {
+router.get("/", async function(req, res, next) {
   try {
     const books = await Book.findAll(req.query);
     return res.json({ books });
@@ -17,7 +20,7 @@ router.get("/", async function (req, res, next) {
 
 /** GET /[id]  => {book: book} */
 
-router.get("/:id", async function (req, res, next) {
+router.get("/:id", async function(req, res, next) {
   try {
     const book = await Book.findOne(req.params.id);
     return res.json({ book });
@@ -28,8 +31,14 @@ router.get("/:id", async function (req, res, next) {
 
 /** POST /   bookData => {book: newBook}  */
 
-router.post("/", async function (req, res, next) {
+router.post("/", async function(req, res, next) {
   try {
+    const result = jsonschema.validate(req.body, bookSchema);
+    if (!result.valid) {
+      const errList = result.errors.map((e) => e.stack);
+      const error = new ExpressError(errList, 400);
+      return next(error);
+    }
     const book = await Book.create(req.body);
     return res.status(201).json({ book });
   } catch (err) {
@@ -39,10 +48,16 @@ router.post("/", async function (req, res, next) {
 
 /** PUT /[isbn]   bookData => {book: updatedBook}  */
 
-router.put("/:isbn", async function (req, res, next) {
+router.put("/:isbn", async function(req, res, next) {
   try {
+    const result = jsonschema.validate(req.body, bookSchema);
+    if (!result.valid) {
+      const errList = result.errors.map((e) => e.stack);
+      const error = new ExpressError(errList, 400);
+      return next(error);
+    }
     const book = await Book.update(req.params.isbn, req.body);
-    return res.json({ book });
+    return res.status(204).json({ book });
   } catch (err) {
     return next(err);
   }
@@ -50,10 +65,10 @@ router.put("/:isbn", async function (req, res, next) {
 
 /** DELETE /[isbn]   => {message: "Book deleted"} */
 
-router.delete("/:isbn", async function (req, res, next) {
+router.delete("/:isbn", async function(req, res, next) {
   try {
     await Book.remove(req.params.isbn);
-    return res.json({ message: "Book deleted" });
+    return res.status(200).json({ message: "Book deleted" });
   } catch (err) {
     return next(err);
   }
